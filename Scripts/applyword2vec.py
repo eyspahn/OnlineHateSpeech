5e3f2004341bb864a23f236255be2f450f6c88e6
Emily Spahn <eyspahn@gmail.com>
2015-12-29 23:22:55 -0800

    Started xgboost

--- Scripts/applyword2vec.py
+++ Scripts/applyword2vec.py
@@ -1,11 +1,12 @@
 import cPickle as pickle
 from gensim.models import Word2Vec
 import re
+import string
 
 
 # Prepare data to run Word2Vec
 # Needs data to be fed in by sentence, which is a list of lists.
-# [ [sentence, 1, words, go, here], [sentence, 2, workds, go, here]]
+# [ [sentence, 1, words, go, here], [sentence, 2, words, go, here] ]
 # http://radimrehurek.com/gensim/models/word2vec.html
 
 
@@ -26,9 +27,10 @@ def separate_data(df):
     GenderHateComments = df[df.label=='GenderHate']
     ReligionHateComments = df[df.label=='ReligionHate']
     RaceHateComments = df[df.label=='RaceHate']
+    AllComments = df.body
 
     return [NotHateComments.body, SizeHateComments.body, GenderHateComments.body,
-            ReligionHateComments.body, RaceHateComments.body]
+            ReligionHateComments.body, RaceHateComments.body, AllComments]
 
 def comment2sentence(comment):
     '''
@@ -42,15 +44,20 @@ def comment2sentence(comment):
 
 def sentence2list(sentence):
     '''
-    Split sentences on white spaces and put into a list.
+    Makes words lowercase, removes punctuation, splits words on white space & returns list.
     '''
+    #make lowercase
+    sentence = sentence.lower()
+    #remove punctuation
+    sentence = ''.join(l for l in sentence if l not in string.punctuation)
 
+    #return split sentence in a list
     return sentence.split()
 
 
 def prepare_comments(comments):
     '''
-    Prepare a set of comments for reading in & processing by
+    Prepare a set of comments for reading in & processing by word2vec
     '''
 
     fullcommentslist = []
@@ -70,23 +77,39 @@ def prepare_comments(comments):
 if __name__ == '__main__':
     print('Loading Data')
     df = load_data()
+
     print('Separating Comments')
-    NotHateComments, SizeHateComments, GenderHateComments, ReligionHateComments, RaceHateComments = separate_data(df)
-    print('Preparing Comments')
-    SizeHatePreparedComments = prepare_comments(SizeHateComments)
+    NotHateComments, SizeHateComments, GenderHateComments, ReligionHateComments,\
+    RaceHateComments, AllComments = separate_data(df)
+
     print('Running SizeHateModel')
-    # SizeHateModel = Word2Vec(SizeHatePreparedComments, size=200, workers=3)
-    # #save model for further use
-    # SizeHateModel.save('../Data/word2vecmodels/SizeHateModel.model')
-    # #SizeHateModel=Word2Vec.load('../Data/word2vecmodels/SizeHateModel.model')
-    NotHateModel = Word2Vec(NotHateComments, size=200, workers=3)
+    SizeHatePreparedComments = prepare_comments(SizeHateComments)
+    SizeHateModel = Word2Vec(SizeHatePreparedComments, size=200, workers=3)
+    SizeHateModel.save('../Data/word2vecmodels/SizeHateModel.model')
+    # SizeHateModel=Word2Vec.load('../Data/word2vecmodels/SizeHateModel.model')
+
+    print('Running NotHateModel')
+    NotHatePreparedComments = prepare_comments(NotHateComments)
+    NotHateModel = Word2Vec(NotHatePreparedComments, size=200, workers=3)
     NotHateModel.save('../Data/word2vecmodels/NotHateModel.model')
 
-    GenderHateModel = Word2Vec(GenderHateComments, size=200, workers=3)
+    print('Running GenderHateModel')
+    GenderHatePreparedComments = prepare_comments(GenderHateComments)
+    GenderHateModel = Word2Vec(GenderHatePreparedComments, size=200, workers=3)
     GenderHateModel.save('../Data/word2vecmodels/GenderHateModel.model')
 
-    ReligionHateModel = Word2Vec(ReligionHateComments, size=200, workers=3)
+    print('Running ReligionHateModel')
+    ReligionHatePreparedComments = prepare_comments(ReligionHateComments)
+    ReligionHateModel = Word2Vec(ReligionHatePreparedComments, size=200, workers=3)
     ReligionHateModel.save('../Data/word2vecmodels/ReligionHateModel.model')
 
-    RaceHateModel = Word2Vec(RaceHateComments, size=200, workers=3)
-    RaceHateModel.save('../Data/word2vecmodels/RaceHateModel.model')    
+    print('Running RaceHateModel')
+    RaceHatePreparedComments = prepare_comments(RaceHateComments)
+    RaceHateModel = Word2Vec(RaceHatePreparedComments, size=200, workers=3)
+    RaceHateModel.save('../Data/word2vecmodels/RaceHateModel.model')
+
+    #All comments
+    print('Running AllCommentsModel')
+    AllCommentsPreparedComments = prepare_comments(AllComments)
+    AllCommentsModel = Word2Vec(AllCommentsPreparedComments, size=300, workers=3)
+    AllCommentsModel.save('../Data/word2vecmodels/AllCommentsModel.model')
